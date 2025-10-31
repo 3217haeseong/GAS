36a9ccebdfb4ab35499e28e9066e86b873699d6b
haeseong <32110495+3217haeseong@users.noreply.github.com>
2025-10-31 16:31:16 +0900

    GAS 3.5

---
+++ Source/ArenaBattleGAS/GA/ABGA_Attack.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GA/ABGA_Attack.h"
+
+void UABGA_Attack::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+}
+
+void UABGA_Attack::InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+}
+
+void UABGA_Attack::CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility)
+{
+	Super::CancelAbility(Handle, ActorInfo, ActivationInfo, bReplicateCancelAbility);
+}
+
+void UABGA_Attack::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
---
+++ Source/ArenaBattleGAS/Prop/ABGASFountain.cpp
@@ -0,0 +1,91 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Prop/ABGASFountain.h"
+#include "ABGASFountain.h"
+#include "GameFramework/RotatingMovementComponent.h"
+#include "ArenaBattleGAS.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayAbilitySpec.h"
+#include "Tag/ABGamePlayTag.h"
+#include "Abilities/GameplayAbility.h"
+//#include "GA/ABGA_Rotate.h"
+
+AABGASFountain::AABGASFountain()
+{
+	ASC = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("ASC"));
+	RotatingMovement = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("RotateMovement"));
+	ActionPeriod = 3.0f;
+}
+
+UAbilitySystemComponent* AABGASFountain::GetAbilitySystemComponent() const
+{
+	return ASC;
+}
+
+void AABGASFountain::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	RotatingMovement->bAutoActivate = false;
+	RotatingMovement->Deactivate();
+
+	ASC->InitAbilityActorInfo(this, this);
+
+	for (const auto& StartAbility : StartAbilities)
+	{
+		FGameplayAbilitySpec StartSpec(StartAbility);
+		ASC->GiveAbility(StartSpec);
+	}
+	
+}
+
+void AABGASFountain::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorld()->GetTimerManager().SetTimer(ActionTimer, this, &AABGASFountain::TimerAction, ActionPeriod, true, 0.0f);
+
+	
+}
+
+void AABGASFountain::TimerAction()
+{
+	ABGAS_LOG(LogABGAS, Log, TEXT("Begin"));
+
+	FGameplayTagContainer TargetTag(ABTAG_ACTOR_ROTATE);
+
+	if (!ASC->HasMatchingGameplayTag(ABTAG_ACTOR_ISROTATING))
+	{
+		ASC->TryActivateAbilitiesByTag(TargetTag);
+	}
+	else
+	{
+		ASC->CancelAbilities(&TargetTag);
+	}
+
+	/*FGameplayAbilitySpec* RotateGASpec = ASC->FindAbilitySpecFromClass(UABGA_Rotate::StaticClass());
+	if (!RotateGASpec)
+	{
+		ABGAS_LOG(LogABGAS, Log, TEXT("No Rotate Spec Found!"));
+		return;
+	}
+
+	if (!RotateGASpec->IsActive())
+	{
+		ASC->TryActivateAbility(RotateGASpec->Handle);
+	}
+	else
+	{
+		ASC->CancelAbilityHandle(RotateGASpec->Handle);
+	}*/
+
+	/*if (!RotatingMovement->IsActive())
+	{
+		RotatingMovement->Activate(true);
+	}
+	else
+	{
+		RotatingMovement->Deactivate();
+	}*/
+}
